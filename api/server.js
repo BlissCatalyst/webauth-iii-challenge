d6cc8d90d335e773c6f9ece7685955135435c51a
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-04 14:56:49 -0600

    guided project backend(to be redone because I still have to finish my own, but I wanted to work on the react stuff today.

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const configureMiddleware = require('./middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+const server = express();
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+module.exports = server;
